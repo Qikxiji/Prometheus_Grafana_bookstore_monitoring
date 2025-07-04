eea9e41cf4f5a943c1253bd6a7ec9a3684a9a8a1
Qikxiji <qikxiji@gmail.com>
2025-07-04 21:14:47 +0300

    init

---
+++ nginx/5-php-pfm_entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#CMD used to launch nginx.
+#this command-script in /docker-entrypoint.d/ will launch php-fpm
+
+php-fpm8.2
---
+++ nginx/bookstore/connectDB.php
@@ -0,0 +1,3 @@
+<?php
+$pdo=new PDO('mysql:host=172.16.16.16;port=3306;dbname=bookstore','root', 'PASSWORD');
+?>
